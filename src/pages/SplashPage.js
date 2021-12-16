44f74b4546f728ab1e687022a0419199a587181d
Greg Munro <gregmunro90@gmail.com>
2021-12-16 10:24:10 +0100

    some stuff m8

--- src/pages/SplashPage.js
+++ src/pages/SplashPage.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SplashPage() {
-    return (
-        <div>
-            <Link to="/home">Barcelona</Link>
-        </div>
-    )
+  return (
+      <div class="h-max bg-gradient-to-r from-lime-600 ... ">
+      <img src="assets/splash logo.png" alt="splash-logo"/>
+      <img className="/assets/Frame 15.png" alt="man"/>
+        <Link to="/home">Get Started</Link>
+    </div>
+  );
 }
